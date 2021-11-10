e6c0a88b43ec76beddeb5919cfb3907ccc5f158d
LuisM03 <luisdavidmontes123@gmail.com>
2021-11-09 22:06:08 -0500

    Actualizacion a la personalización del home

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config.from_object("config.productionConfig")
+app.config.from_object("config.developmentConfig")
 
 db = SQLAlchemy(app)
 mg = Migrate(app, db)
