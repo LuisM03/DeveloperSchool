524046bd45a57729cdfe56d784394b85ee44ab46
LuisM03 <luisdavidmontes123@gmail.com>
2021-11-19 16:30:36 -0500

    UPGRADE archive route

--- app/routes/formRegisterandLogin.py
+++ app/routes/formRegisterandLogin.py
@@ -1,12 +1,33 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, request
 from app.schemas.form import formRegister
+
 
 formRegisterandLogin = Blueprint('formRegisterandLogin', __name__, template_folder='templates')
 
-@formRegisterandLogin.route('/register')
+
+@formRegisterandLogin.route('/register', methods=['GET', 'POST'])
 def register():
     formReg = formRegister()
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        lastname = request.form.get('lastname')
+        documentsType = request.form.get('documentsType')
+        documentsNumber = request.form.get('documentsNumber')
+        email = request.form.get('email')
+        age = request.form.get('age')
+        gender = request.form.get('gender')
+        password = request.form.get('password')
+        rePassword = request.form.get('rePassword')
+
+        if formReg.validate_on_submit():
+            return 'success' ###Por ahora lo vamos  a dejar que nos retorne ese mensaje
+        else:
+            return 'Los parametros son incorrectos' ###Al igual que este mensaje.
+        
     return render_template('register.html', formReg = formReg)
+
+
 
 @formRegisterandLogin.route('/login')
 def login():
--- app/schemas/form.py
+++ app/schemas/form.py
@@ -8,11 +8,11 @@ from wtforms.validators import DataRequired, EqualTo, Required
 class formRegister(FlaskForm):
     name = StringField('Nombre', validators=[DataRequired(), Required('Introduce tu nombre')], render_kw={"placeholder": "Nombre"})
     lastname = StringField('Apellidos', validators=[DataRequired(), Required('Ingrese sus apellidos')], render_kw={"placeholder": "Apellidos"})
-    documentType = SelectField('Tipo de documento', choices=[('Cédula de Ciudadanía'),('Targeta de Identidad'), ('Pasaporte'), ('Licencia de Conducir')], render_kw={"placeholder": "Tipo de documento"})
+    documentType = SelectField('Tipo de documento', choices=[('Cédula de Ciudadanía'),('Targeta de Identidad'), ('Pasaporte'), ('Licencia de Conducir')])
     documentNumber = IntegerField('Número de documento', validators=[DataRequired(), Required('Introduzca su número de documento')], render_kw={"placeholder": "Número de documento"})
     email = EmailField('Correo', validators=[DataRequired(), validators.Email('Ingrese su correo electónico')], render_kw={"placeholder": "Correo"})
     age = IntegerField('Edad', validators=[DataRequired(), Required('Ingrese su edad')], render_kw={"placeholder": "Edad"})
-    gender = SelectField('Genero', choices=[('Hombre'), ('Mujer')], render_kw={"placeholder": "Sexo"})
+    gender = SelectField('Genero', choices=[('Hombre'), ('Mujer')])
     password = PasswordField('Contraseña', validators=[DataRequired(), Required('Ingrese una contraseña')], render_kw={"placeholder": "Contraseña"})
     rePassword = PasswordField('Reafirmar Contraseña', validators=[DataRequired(), EqualTo('password', message='Las contraseñas no coinciden')], render_kw={"placeholder": "Confirmar contraseña"})
     submit = SubmitField('Continuar')
